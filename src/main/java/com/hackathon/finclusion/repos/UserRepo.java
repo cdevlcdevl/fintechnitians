182b64e2d29c1c0bd448bce305a8215b3f091a3e
cdevlcdevl <cdevlcdevl@gmail.com>
2025-07-22 12:04:12 -0400

    added extra data access methods

--- src/main/java/com/hackathon/finclusion/repos/UserRepo.java
+++ src/main/java/com/hackathon/finclusion/repos/UserRepo.java
@@ -2,9 +2,14 @@ package com.hackathon.finclusion.repos;
 
 import com.hackathon.finclusion.models.Account;
 import com.hackathon.finclusion.models.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class UserRepo {
@@ -18,10 +23,36 @@ public class UserRepo {
         String sql = "SELECT name, email FROM users";
         return jdbcTemplate.query(
                 sql,
-                (rs, rowNum) -> new User(
-                        rs.getString(USER_NAME_COLUMN),
-                        rs.getString(USER_EMAIL_COLUMN)
-                )
+                (rs, rowNum) -> getUserFromResults(rs)
+        );
+    }
+
+    public Optional<User> findByName(String name) {
+        String sql = "SELECT name, email FROM users WHERE name = ?";
+        try {
+            User user = jdbcTemplate.queryForObject(
+                    sql,
+                    (rs, rowNum) -> getUserFromResults(rs),
+                    name // This argument is passed to the '?' placeholder
+            );
+            return Optional.ofNullable(user);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+    public List<User> findByEmail(String email) {
+        String sql = "SELECT name, email FROM users WHERE email = ?";
+        return jdbcTemplate.query(
+                sql,
+                (rs, rowNum) -> getUserFromResults(rs),
+                email
+        );
+    }
+
+    private User getUserFromResults(ResultSet rs) throws SQLException {
+        return new User(
+                rs.getString(USER_NAME_COLUMN),
+                rs.getString(USER_EMAIL_COLUMN)
         );
     }
 
